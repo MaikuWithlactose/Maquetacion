fe025b70a9148556d5b3b0b4945876fa2d44d3e6
MaikuWithlactose <131139597+MaikuWithlactose@users.noreply.github.com>
2023-05-17 13:01:35 +0200

    reto agencia

---
+++ agenciaindex.js
@@ -0,0 +1,42 @@
+
+let solicitudes = [];
+
+
+function enviarForm(event) {
+    event.preventDefault(); 
+    const nombre = document.getElementById("nombre").value;
+    const origen = document.getElementById("origen").value;
+    const destino = document.getElementById("destino").value;
+    const personas = document.getElementById("personas").value;
+    const fecha = document.getElementById("fechas").value;
+
+    const solicitud = {
+        nombre, origen, destino, personas, fechas,
+    };
+   
+
+    solicitudes.push(solicitud)
+
+    console.log("se ha enviado");
+
+   
+    document.querySelector("form").reset();
+
+   
+    for (let i = 0; i < solicitudes.length; i++) {
+}
+};
+
+function filtrarSol() {
+    const destinosPermitidos = ["canarias", "mallorca", "galicia"];
+    let datosFiltrados = "";
+
+    for (let i = 0; i < solicitudes.length; i++) {
+        let destino = solicitudes[i].destino.toLowerCase();
+        if (destinosPermitidos.includes(destino)) {
+            console.log(solicitudes[i]);
+            datosFiltrados += 'Nombre: ${solicitudes[i].nombre}, Origen: ${solicitudes[i].origen} Destino: ${solicitudes[i].destino} Personas: ${solicitudes[i].personas} Fechas: ${solicitudes[i].fecha}<br>';
+        }
+    }
+    document.getElementById("datosFiltrados").innerHTML = datosFiltrados;
+}
